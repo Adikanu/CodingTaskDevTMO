31c410b0661ce7b3e30d4bf9006fc172a905f6e4
Aaditya <aaditya.kps127@gmail.com>
2021-01-21 15:51:38 -0500

    e2e test implemented for undo action task 3

--- web-ui-developer-puzzle-master/apps/okreads-e2e/src/specs/reading-list.spec.ts
+++ web-ui-developer-puzzle-master/apps/okreads-e2e/src/specs/reading-list.spec.ts
@@ -1,5 +1,17 @@
 import { $, browser, ExpectedConditions } from 'protractor';
 
+// tslint:disable-next-line:nx-enforce-module-boundaries
+import * as ReadingListActions from 'libs/books/data-access/src/lib/+state/reading-list.actions';
+
+// tslint:disable-next-line:nx-enforce-module-boundaries
+import {
+    initialState,
+    readingListAdapter,
+    reducer,
+    State
+} from 'libs/books/data-access/src/lib/+state/reading-list.reducer';
+import { createBook, createReadingListItem } from '@tmo/shared/testing';
+
 describe('When: I use the reading list feature', () => {
   it('Then: I should see my reading list', async () => {
     await browser.get('/');
@@ -17,4 +29,63 @@ describe('When: I use the reading list feature', () => {
       )
     );
   });
+ 
+
+  describe('Reading List  Reducer', () => {
+    describe('valid Reading List actions', () => {
+      let state: State;
+  
+      beforeEach(() => {
+        state = readingListAdapter.setAll(
+          [createReadingListItem('A'), createReadingListItem('B')],
+          initialState
+        );
+      });
+
+      it('loadBooksSuccess should load books from reading list', () => {
+        const list = [
+          createReadingListItem('A'),
+          createReadingListItem('B'),
+          createReadingListItem('C')
+        ];
+        const action = ReadingListActions.loadReadingListSuccess({ list });
+  
+        const result: State = reducer(initialState, action);
+  
+        expect(result.loaded).toBe(true);
+        expect(result.ids.length).toEqual(3);
+      });
+
+      it('UndoAddToReadingList should undo book addition to the state', () => {
+        const action = ReadingListActions.undoAddToReadingList({
+          book: createBook('B')
+        });
+  
+        const result: State = reducer(state, action);
+  
+        expect(result.ids).toEqual(['A']);
+      });
+  
+
+      it('UndoRemoveFromReadingList should undo book removal from the state', () => {
+        const action = ReadingListActions.undoRemoveFromReadingList({
+          item: createReadingListItem('C')
+        });
+  
+        const result: State = reducer(state, action);
+         
+        expect(result.ids).toEqual(['A', 'B' , 'C']);
+      });
+    });
+  
+    describe('unknown action', () => {
+      it('should return the previous state', () => {
+        const action = {} as any;
+  
+        const result = reducer(initialState, action);
+  
+        expect(result).toEqual(initialState);
+      });
+    });
+  })
 });
--- web-ui-developer-puzzle-master/libs/books/data-access/src/lib/+state/reading-list.reducer.spec.ts
+++ web-ui-developer-puzzle-master/libs/books/data-access/src/lib/+state/reading-list.reducer.spec.ts
@@ -49,7 +49,7 @@ describe('Books Reducer', () => {
 
       const result: State = reducer(state, action);
        
-      expect(result.ids).toEqual(['A', 'B' , 'C']);
+      expect(result.ids).toEqual(['A', 'B', 'C']);
     });
   });
 
